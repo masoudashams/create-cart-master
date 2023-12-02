9bee40b6a8f0d5dc01705f11c670b88b3f9a7b1d
masoudashams <masouda.shams1722@gmail.com>
2023-12-01 18:23:25 -0800

    first commit

---
+++ react-cart-master/src/components/Home.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+
+const img1 =
+  "https://www.reliancedigital.in/medias/Apple-MGN63HNA-Laptops-491946461-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxNzczNDJ8aW1hZ2UvanBlZ3xpbWFnZXMvaDVhL2gyZC85NDQzMDgzNTgzNTE4LmpwZ3xhYzRiNWIxZGQ2NjNiNWIyYjI0Y2ZkYTZlZWQ3MTFjZTMxYzVmNDBiNmM5Mzk5OTM2OGVkZmExMjMyYjIxNDQ4";
+const img2 =
+  "https://admin.walkwayshoes.com/product/201-1141/1080/201-1141M11.jpg";
+  const img3=
+  "https://m.media-amazon.com/images/I/71sYkdV+D7L._SL1500_.jpg";
+
+const Home = () => {
+  const productList = [
+    {
+      name: "Mac Book",
+      price: 12000,
+      imgSrc: img1,
+      id: "asdhajsdbhjabhsjdfdfv",
+    },
+    {
+      name: "BlackShoes",
+      price: 90,
+      imgSrc: img2,
+      id: "sdjfdlaajsdbhjabhsjdfdfv",
+    },
+    {
+      name: "shirt for men",
+      price: 190,
+      imgSrc: img3,
+      id: "sdjfdlaajsdbhjabhsjdfdfv",
+    },
+  ];
+
+  const dispatch = useDispatch();
+
+  const addToCartHandler = (options) => {
+    dispatch({ type: "addToCart", payload: options });
+    dispatch({ type: "calculatePrice" });
+    toast.success("Added To Cart");
+  };
+  return (
+    <div className="home">
+      {productList.map((i) => (
+        <ProductCard
+          key={i.id}
+          imgSrc={i.imgSrc}
+          name={i.name}
+          price={i.price}
+          id={i.id}
+          handler={addToCartHandler}
+        />
+      ))}
+    </div>
+  );
+};
+
+const ProductCard = ({ name, id, price, handler, imgSrc }) => (
+  <div className="productCard">
+    <img src={imgSrc} alt={name} />
+    <p>{name}</p>
+    <h4>${price}</h4>
+    <button onClick={() => handler({ name, price, id, quantity: 1, imgSrc })}>
+      Add to Cart
+    </button>
+  </div>
+);
+
+export default Home;
